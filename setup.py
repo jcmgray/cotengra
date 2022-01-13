9c34c06f55709cf0154981c38c0da4f8925f3805
Johnnie Gray <johnniemcgray@gmail.com>
2022-01-13 12:31:38 -0800

    add missing docs reqs to setup.py

--- setup.py
+++ setup.py
@@ -40,6 +40,12 @@ setup(
             'autoray',
             'ray',
         ],
+        'docs': [
+            'sphinx',
+            'sphinx-autoapi',
+            'myst-nb',
+            'furo',
+        ],
         'test': [
             'numpy',
             'kahypar',
