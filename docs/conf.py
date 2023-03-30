20fdf6e5fc2e803bd159072278918d6745885405
Johnnie Gray <johnniemcgray@gmail.com>
2023-03-30 13:30:08 -0700

    tweak conf.py [no ci]

--- docs/conf.py
+++ docs/conf.py
@@ -1,29 +1,20 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-import sys, os
+import os
+import sys
 sys.path.append(os.path.abspath("./_pygments"))
 
 
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'cotengra'
 copyright = '2020-2022, Johnnie Gray'
 author = 'Johnnie Gray'
 
-# The full version, including alpha/beta/rc tags
 try:
     from cotengra import __version__
     release = __version__
@@ -36,10 +27,8 @@ except ImportError:
 
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     'myst_nb',
     'sphinx.ext.intersphinx',
@@ -63,22 +52,14 @@ myst_enable_extensions = [
 # sphinx-autoapi
 autoapi_dirs = ['../cotengra']
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'furo'
-# html_theme = 'sphinx_book_theme'
 
 html_theme_options = {
     "sidebar_hide_name": True,
@@ -97,9 +78,6 @@ html_theme_options = {
 pygments_style = '_pygments_light.MarianaLight'
 pygments_dark_style = "_pygments_dark.MarianaDark"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_css_files = ["my-styles.css"]
 html_favicon = "_static/logo-favicon.ico"
@@ -155,6 +133,6 @@ def linkcode_resolve(domain, info):
         )
     else:
         return (
-            f"https://github.com/pydata/cotengra/blob/"
+            f"https://github.com/jcmgray/cotengra/blob/"
             f"v{cotengra.__version__}/cotengra/{fn}{linespec}"
         )
