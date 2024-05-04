008564bcb5275b9c62a0ada39b57cefd6db916e8
Christoph Staudt <christoph.staudt@uni-jena.de>
2024-05-05 00:15:16 +0200

    Reduced iterative version with contract trees

--- cotengra/pathfinders/path_kahypar_plus.py
+++ cotengra/pathfinders/path_kahypar_plus.py
@@ -2,25 +2,20 @@
 # Based on https://github.com/ti2-group/hybrid_contraction_tree_optimizer/
 #
 
-import copy
 import math
 import random
 import uuid
 from collections import defaultdict
 from dataclasses import dataclass, field
-from functools import total_ordering
-from queue import PriorityQueue
 from typing import (
     Callable,
     Hashable,
     Optional,
-    Sequence,
     Union,
     List,
     Tuple,
     Dict,
     Set,
-    FrozenSet,
 )
 
 from cotengra.pathfinders.path_basic import (
@@ -33,8 +28,7 @@ from ..hyperoptimizers.hyper import register_hyper_function
 
 
 Inputs = List[List[Hashable]]
-Shape = Tuple[int, ...]
-Shapes = List[Shape]
+
 Output = List[Hashable]
 SizeDict = Dict[Hashable, int]
 Path = List[Tuple[int, ...]]
@@ -44,7 +38,6 @@ GreedyOptimizer = Callable[["TensorNetwork"], Path]
 @dataclass
 class BasicInputNode:
     indices: List[Hashable]
-    shape: Shape
 
 
 @dataclass
@@ -55,18 +48,18 @@ class OriginalInputNode(BasicInputNode):
         return str(self.id)
 
     def __repr__(self) -> str:
-        return f"Original Input({self.indices}, {self.shape})"
+        return f"Original Input({self.indices})"
 
 
 @dataclass
 class SubNetworkInputNode(BasicInputNode):
-    sub_network: "SubTensorNetwork"
+    sub_network: "TensorNetwork"
 
     def get_id(self):
         return f"sn-{self.sub_network.name}"
 
     def __repr__(self) -> str:
-        return f"Sub network Input({self.sub_network.output_indices}, {self.sub_network.get_output_shape()})"
+        return f"Sub network Input({self.sub_network.output_indices},"
 
 
 InputNode = Union[OriginalInputNode, SubNetworkInputNode]
@@ -85,7 +78,10 @@ class IntermediateContractNode:
         return self.uuid
 
     def __repr__(self) -> str:
-        return f"IntermediateContractNode({self.uuid}), children: {[child.get_id() for child in self.children]}"
+        return (
+            f"IntermediateContractNode({self.uuid}), "
+            + f"children: {[child.get_id() for child in self.children]}"
+        )
 
 
 ContractTreeNode = Union[
@@ -106,7 +102,7 @@ def get_contract_tree_and_cost_from_path(
 ) -> Tuple[ContractTree, int]:
     contract_tree: ContractTree = []
     histogram = defaultdict(lambda: 0)
-    for input in tn.get_all_input_nodes():
+    for input in tn.inputs:
         contract_tree.append(input)
         for edge in input.indices:
             histogram[edge] += 1
@@ -178,7 +174,7 @@ def get_contract_tree_and_cost_from_path(
 
 
 def greedy_optimizer(tn: "TensorNetwork") -> Path:
-    inputs = [input.indices for input in tn.get_all_input_nodes()]
+    inputs = [input.indices for input in tn.inputs]
     output = tn.output_indices
     size_dict = tn.size_dict
 
@@ -191,225 +187,44 @@ def greedy_optimizer(tn: "TensorNetwork") -> Path:
 
 
 @dataclass
-class SubTensorNetwork:
+class TensorNetwork:
     name: str
-    key: int
     parent_name: str
     inputs: InputNodes
-    indices: FrozenSet[Hashable]
     size_dict: SizeDict
-    cut_indices: FrozenSet[Hashable]
     output_indices: Output
-
-    def get_all_input_nodes(self) -> InputNodes:
-        return self.inputs
-
-    def get_all_networks(self):
-        return [self]
-
-    def get_output_shape(self):
-        return tuple([self.size_dict[index] for index in self.output_indices])
-
-    def find_path(self):
-        """
-        Finds the path for the sub-tensor network.
-
-        Returns:
-            SubTensorNetworkWithContractTree: The sub-tensor network with the computed contract tree and its cost.
-        """
-        path = greedy_optimizer(self)
-
-        contract_tree, cost = get_contract_tree_and_cost_from_path(self, path)
-
-        tn_with_tree = SubTensorNetworkWithContractTree(
-            name=self.name,
-            key=self.key,
-            parent_name=self.parent_name,
-            inputs=self.inputs,
-            indices=self.indices,
-            size_dict=self.size_dict,
-            cut_indices=self.cut_indices,
-            output_indices=self.output_indices,
-            cost=cost,
-            contract_tree=contract_tree,
-        )
-        return tn_with_tree
-
-
-@total_ordering
-@dataclass
-class SubTensorNetworkWithContractTree(SubTensorNetwork):
-    cost: int
-    contract_tree: ContractTree
-
-    def get_total_cost(self):
-        return self.cost
+    _contract_tree: ContractTree = None
+    _cost: Optional[int] = None
+    _ssa_id: Optional[int] = None
 
     def get_contract_tree(self):
-        return self.contract_tree
-
-    def __eq__(self, other):
-        if not isinstance(other, __class__):
-            return NotImplemented
-        return self.cost == other.cost
-
-    def __lt__(self, other):
-        if not isinstance(other, __class__):
-            return NotImplemented
-        # Yes this might seem weird, but we want the one with the highest cost to be the first in the priority queue
-        return self.cost > other.cost
+        if self._contract_tree is None:
+            self.find_path()
+        return self._contract_tree
 
     def find_path(self):
         """
-        Since the path is already computed, this function just returns the current object.
-
-        Returns:
-            The current object.
+        Finds the path for the tensor network.
         """
-        return self
-
-
-@dataclass
-class SuperTensorNetwork(SubTensorNetwork):
-    parent_name: str
-    sub_networks: Sequence[SubTensorNetwork]
-
-    def get_all_input_nodes(self) -> InputNodes:
-        sub_input_nodes = [
-            SubNetworkInputNode(
-                sub_network.output_indices,
-                sub_network.get_output_shape(),
-                sub_network,
-            )
-            for sub_network in self.sub_networks
-        ]
-
-        return sub_input_nodes + self.inputs
-
-    def get_all_networks(self):
-        return [self] + [sub_network for sub_network in self.sub_networks]
-
-    def find_path(self):
-        sub_networks_with_path: List[SubTensorNetworkWithContractTree] = []
-        for sub_network in self.sub_networks:
-            sub_networks_with_path.append(sub_network.find_path())
-
-        self.sub_networks = sub_networks_with_path
-
         path = greedy_optimizer(self)
         contract_tree, cost = get_contract_tree_and_cost_from_path(self, path)
-
-        tn_with_tree = SuperTensorNetworkWithTree(
-            self.name,
-            self.key,
-            self.parent_name,
-            self.inputs,
-            self.indices,
-            self.size_dict,
-            self.cut_indices,
-            self.output_indices,
-            cost,
-            contract_tree,
-            sub_networks_with_path,
-        )
-
-        return tn_with_tree
-
-
-@dataclass
-class SuperTensorNetworkWithTree(
-    SuperTensorNetwork,
-    SubTensorNetworkWithContractTree,
-):
-    sub_networks: List[SubTensorNetworkWithContractTree]
-
-    # def find_path(self, greedy_optimizer: GreedyOptimizer):
-    #     """
-    #     Since the path is already computed, this function just returns the current object.
-
-    #     Returns:
-    #         The current object.
-    #     """
-    #     return self
-
-    def get_total_cost(self):
-        return (
-            sum([sub_network.cost for sub_network in self.sub_networks])
-            + self.cost
-        )
-
-    def get_parent_tree(self):
-        super_tree = self.get_contract_tree()
-
-        parent_tree = []
-        sub_tree_root: Dict[str, ContractTreeNode] = {}
-        for node in super_tree:
-            if (
-                isinstance(node, SubNetworkInputNode)
-                and node.sub_network.parent_name == self.name
-            ):
-                assert isinstance(
-                    node.sub_network, SubTensorNetworkWithContractTree
-                ), "The subnetworks should have a contract tree, when calling get_parent_tree"
-                sub_tree = None
-                for sn in self.sub_networks:
-                    if sn.name == node.sub_network.name:
-                        sub_tree = sn.contract_tree
-                assert (
-                    sub_tree is not None
-                ), f"Sub tree {node.sub_network.name} not found in {self.name}"
-                for sub_node in sub_tree:
-                    parent_tree.append(sub_node)
-
-                sub_tree_root[node.sub_network.name] = sub_tree[-1]
-            elif isinstance(node, IntermediateContractNode):
-                for key, child in enumerate(node.children):
-                    if (
-                        isinstance(child, SubNetworkInputNode)
-                        and child.sub_network.parent_name == self.name
-                    ):
-                        node.children[key] = sub_tree_root[
-                            child.sub_network.name
-                        ]
-
-                parent_tree.append(node)
-            else:
-                parent_tree.append(node)
-
-        return parent_tree
-
-    def update_tree(self, name, tree: ContractTree):
-        if name == self.name:
-            self.contract_tree = tree
-        else:
-            for sub_network in self.sub_networks:
-                if sub_network.name == name:
-                    sub_network.contract_tree = tree
-                    return
-            raise Exception(f"name {name} not found in {self.name}")
-
-    def find_path(self):
-        return self
-
-
-TensorNetwork = Union[SubTensorNetwork, SuperTensorNetwork]
-TensorNetworkWithTree = Union[
-    SubTensorNetworkWithContractTree, SuperTensorNetworkWithTree
-]
+        self._contract_tree = contract_tree
+        self._cost = cost
 
 
 def get_sub_networks(
-    tensor_network_name: str,
-    input_nodes,
-    output,
-    size_dict: SizeDict,
+    tensor_network: TensorNetwork,
     imbalance: float,
     weight_nodes: str = "const",
 ):
+    input_nodes = tensor_network.inputs
+    output = tensor_network.output_indices
+    size_dict = tensor_network.size_dict
+    tensor_network_name = tensor_network.name
     num_input_nodes = len(input_nodes)
     assert (
         num_input_nodes > 2
-    ), f"Not enough input nodes to split, pass at least two input nodes, {input_nodes}"
+    ), f"You need to pass at least two input nodes, {input_nodes}"
 
     inputs = [input.indices for input in input_nodes]
 
@@ -468,104 +283,54 @@ def get_sub_networks(
 
     cut_indices = frozenset(cut_indices.union(output))
     output_indices: list[Output] = [
-        cut_indices.intersection(block) for block in block_indices
+        list(cut_indices.intersection(block)) for block in block_indices
     ]
 
-    output_block_id = 0
     if len(output) > 0:
         output_block_id = block_ids[-1]
     else:
         output_block_id = random.choice([0, 1])
 
     sub_networks = [
-        SubTensorNetwork(
+        TensorNetwork(
             f"{tensor_network_name}.{key}",
-            key,
-            None,
+            tensor_network_name,
             block_inputs[key],
             block_indices[key],
             size_dict,
-            cut_indices,
             output_indices[key],
         )
         for key, _ in enumerate(block_inputs)
     ]
 
-    super_sub_network = sub_networks.pop(output_block_id)
-    super_network = build_super_network(
-        super_sub_network,
-        tensor_network_name,
-        cut_indices,
-        output,
-        sub_networks,
+    parent_sub_network = sub_networks.pop(output_block_id)
+    child_sub_network = sub_networks.pop()
+    child_sub_network.parent_name = parent_sub_network.name
+    sub_network_node = SubNetworkInputNode(
+        child_sub_network.output_indices,
+        child_sub_network,
     )
+    parent_sub_network.inputs.append(sub_network_node)
+    parent_sub_network.output_indices = output
 
-    return super_network.find_path()
-
-
-def build_super_network(
-    super_network: TensorNetwork,
-    parent_name: str,
-    cut_indices: FrozenSet[Hashable],
-    output: Output,
-    sub_networks: List[SubTensorNetwork],
-):
-    # Set parent for sub_networks
-    for sub_network in sub_networks:
-        sub_network.parent_name = super_network.name
-
-    super_network = SuperTensorNetwork(
-        name=super_network.name,
-        key=super_network.key,
-        parent_name=parent_name,
-        inputs=super_network.inputs,
-        indices=super_network.indices,
-        size_dict=super_network.size_dict,
-        cut_indices=cut_indices,
-        output_indices=output,
-        sub_networks=sub_networks,
-    )
-    return super_network
+    return parent_sub_network, child_sub_network
 
 
-def get_remapped_id(id, input_remap: Optional[Dict[str, int]]):
-    return input_remap[id] if input_remap != None and id in input_remap else id
+def contract_tree_to_path(root: ContractTreeNode, last_id, path: Path):
+    if isinstance(root, IntermediateContractNode):
+        sub_ids = []
+        for child in root.children:
+            sub_root = contract_tree_to_path(child, last_id, path)
+            last_id = max(last_id, sub_root)
+            sub_ids.append(sub_root)
+        path.append(tuple(sub_ids))
+        new_root_id = max(last_id, max(sub_ids)) + 1
+        return new_root_id
 
-
-def contract_tree_to_path(
-    tree: ContractTree, remap: Optional[Dict[str, int]] = None
-):
-    root = tree[-1]
-
-    if isinstance(root, BasicInputNode):
-        assert (
-            len(tree) == 1
-        ), "Tree should only contain one node, if root is basic input"
-        return [(int(get_remapped_id(root.get_id(), remap)),)]
-
-    path = []
-
-    counter = (len(tree) + 1) // 2
-    uuid_to_ssa_id = {}
-    for node in tree:
-        if isinstance(node, IntermediateContractNode):
-            uuid_to_ssa_id[node.get_id()] = counter
-            counter += 1
-            pair = []
-            if isinstance(node.children[0], BasicInputNode):
-                pair.append(get_remapped_id(node.children[0].get_id(), remap))
-            else:
-                pair.append(uuid_to_ssa_id[node.children[0].get_id()])
-
-            if len(node.children) > 1:
-                if isinstance(node.children[1], BasicInputNode):
-                    pair.append(
-                        get_remapped_id(node.children[1].get_id(), remap)
-                    )
-                else:
-                    pair.append(uuid_to_ssa_id[node.children[1].get_id()])
-            path.append(tuple(pair))
-    return path
+    if isinstance(root, OriginalInputNode):
+        return int(root.get_id())
+    if isinstance(root, SubNetworkInputNode):
+        return root.sub_network._ssa_id
 
 
 def hybrid_hypercut_greedy(
@@ -576,80 +341,57 @@ def hybrid_hypercut_greedy(
     weight_nodes="const",
     cutoff=15,
 ):
-    # Problem setup, transform arguments to tanser network with path
-    shapes = [tuple([size_dict[i] for i in input]) for input in inputs]
+    # Noramlize parameters
     inputs = [list(input) for input in inputs]
     output = list(output)
-    indices = frozenset.union(*[frozenset(input) for input in inputs])
 
     input_nodes: InputNodes = [
-        OriginalInputNode(in_sh[0], in_sh[1], id)
-        for id, in_sh in enumerate(zip(inputs, shapes))
+        OriginalInputNode(input, id) for id, input in enumerate(inputs)
     ]
 
-    tensor_network_without_path = SubTensorNetwork(
-        "tn", 0, None, input_nodes, indices, size_dict, frozenset(), output
-    )
+    tensor_network = TensorNetwork("tn", None, input_nodes, size_dict, output)
 
-    tensor_network = tensor_network_without_path.find_path()
-
-    root_name = tensor_network.name
-
-    # Initialize queues
-    to_partition: PriorityQueue[TensorNetwork] = PriorityQueue()
-    to_partition.put(tensor_network)
-
-    # Initialize dictoionary for finalized partitions
-    partitioned: Dict[str, TensorNetworkWithTree] = {}
-
-    while not to_partition.empty():
-        next_network = to_partition.get()
-        if len(next_network.inputs) <= cutoff:
-            with_path = next_network.find_path()  #
-            partitioned[next_network.name] = with_path
+    stack = [tensor_network]
+    path = []
+    last_id = len(inputs) - 1
+    network_by_name = {tensor_network.name: tensor_network}
+    cost = 0
+    while stack:
+        tn = stack.pop()
+        # print(f"Popped {tn.name}")
+        if len(tn.inputs) <= cutoff:
+            tree = tn.get_contract_tree()
+            cost += tn._cost
+            sub_id = contract_tree_to_path(tree[-1], last_id, path)
+            last_id = max(last_id, sub_id)
+            tn._ssa_id = sub_id
+            while tn.parent_name and len(tn.parent_name) < len(tn.name):
+                network_by_name[tn.parent_name]._ssa_id = sub_id
+                tn = network_by_name[tn.parent_name]
             continue
-
-        super_network = get_sub_networks(
-            next_network.name,
-            next_network.get_all_input_nodes(),
-            next_network.output_indices,
-            next_network.size_dict,
+        parent_sub_network, child_sub_network = get_sub_networks(
+            tn,
             imbalance=imbalance,
             weight_nodes=weight_nodes,
         )
+        stack.append(parent_sub_network)
+        network_by_name[parent_sub_network.name] = parent_sub_network
+        # print(f"Pushed parent {parent_sub_network.name}")
+        stack.append(child_sub_network)
+        network_by_name[child_sub_network.name] = child_sub_network
+        # print(f"Pushed child {child_sub_network.name}")
 
-        partitioned[super_network.parent_name] = copy.deepcopy(super_network)
-
-        to_partition.put(super_network)
-
-        for sub_network in super_network.sub_networks:
-            to_partition.put(sub_network)
-
-    def merge(network_name) -> ContractTree:
-        partitioned_network = partitioned[network_name]
-        # Check if we reached a leave
-        if partitioned_network.name == network_name or not (
-            isinstance(partitioned_network, SuperTensorNetworkWithTree)
-        ):
-            with_tree = partitioned_network.find_path()
-            return with_tree.contract_tree
-        for sub_network in partitioned_network.get_all_networks():
-            merged_sub_tree = merge(sub_network.name)
-            partitioned_network.update_tree(sub_network.name, merged_sub_tree)
-
-        parent_tree = partitioned_network.get_parent_tree()
-
-        return parent_tree
-
-    parent_tree = merge(root_name)
-    path = contract_tree_to_path(parent_tree)
+    # contract_tree = tensor_network.build_tree(imbalance, cutoff, weight_nodes)
+    # path = []
+    # contract_tree_to_path(contract_tree[-1], len(inputs) - 1, path)
     path = [tuple([int(i) for i in pair]) for pair in path]
+    # print(f"{cost:.6e}", math.log10(cost / 2), imbalance, cutoff, weight_nodes)
     return ContractionTree.from_path(inputs, output, size_dict, ssa_path=path)
 
 
 hyper_space = {
-    "imbalance": {"type": "FLOAT", "min": 0.01, "max": 0.2},
-    "weight_nodes": {"type": "STRING", "options": ["const", "log"]},
-    "cutoff": {"type": "INT", "min": 40, "max": 200},
+    "imbalance": {"type": "FLOAT", "min": 0.001, "max": 0.1},
+    "weight_nodes": {"type": "STRING", "options": ["log"]},
+    "cutoff": {"type": "INT", "min": 50, "max": 100},
 }
-register_hyper_function("random-kahypar+", hybrid_hypercut_greedy, hyper_space)
+register_hyper_function("kahypar+", hybrid_hypercut_greedy, hyper_space)
